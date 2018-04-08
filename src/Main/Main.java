1785381c0e9dd2f1852203f301d512d943ac8d86
selman <selman.alp@hotmail.com.tr>
2018-04-08 13:14:28 +0300

    Update

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,8 +1,29 @@
 package Main;
 
+
+import com.google.gson.Gson;
+
 public class Main {
 
+
+    // I will use composite pattern to handle  all states and their value
+    /*
+              S
+             /\
+            /  \
+           A    B
+          /      \
+         /        \
+        a          b
+
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Gson gson = new Gson();
+
+
+
     }
+
 }
